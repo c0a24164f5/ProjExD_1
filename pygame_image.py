e3e3b1449bd1de7d0d491fcc46fe129fc3705269
c0a24164f5 <c0a24164f5@edu.teu.ac.jp>
2025-06-10 15:57:04 +0900

    練習9

--- pygame_image.py
+++ pygame_image.py
@@ -20,14 +20,15 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        
-        screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg_img2, [-tmr+1600, 0])
+        x=tmr%3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img,[300,200])
         
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(900)
 
 
 
